da6344cb8d2cad0020ad1c637f0bdfeb488f1452
duo le Sun <1091282902@qq.com>
2025-06-08 21:58:52 +0800

    fix Account iisue

--- Backend/anchor/aimax/programs/aimax/src/receiver.rs
+++ Backend/anchor/aimax/programs/aimax/src/receiver.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint,TokenAccount};
+//use anchor_spl::token::{Mint,TokenAccount};
+use anchor_spl::token_interface::{TokenAccount,Mint,TokenInterface};
 
 //生成seed和bump，实现不同的PDA地址,便于验证地址的唯一性
 pub const EXTERNAL_EXECUTION_CONFIG_SEED: &[u8] = b"external_execution_config";
@@ -7,13 +8,6 @@ pub const APPROVED_SENDER_SEED: &[u8] = b"approved_ccip_sender";
 pub const TOKEN_ADMIN_SEED: &[u8] = b"receiver_token_admin";
 pub const ALLOWED_OFFRAMP: &[u8] = b"allowed_offramp"; //允许
 pub const STATE: &[u8] = b"state";
-
-
-
-
-
-
-
 
 
 const ANCHOR_DISCRIMINATOR: usize = 8;
