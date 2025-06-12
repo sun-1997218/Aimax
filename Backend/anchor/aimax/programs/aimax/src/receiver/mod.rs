aa51ce59078d28913eaf7555835e4b082fe66926
duo le Sun <1091282902@qq.com>
2025-06-12 15:14:17 +0800

    reprogramed

--- Backend/anchor/aimax/programs/aimax/src/receiver/mod.rs
+++ Backend/anchor/aimax/programs/aimax/src/receiver/mod.rs
@@ -1,1 +1,8 @@
 pub mod receiver;
+pub mod get_lastest_messages;
+pub mod withdrwa_tokens;
+pub mod state;
+pub mod context;
+pub mod error;
+pub mod events;
+pub use receiver::handler as ccip_receiver_handler;
