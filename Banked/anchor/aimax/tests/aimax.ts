57392944be2831efe363fa160f2d8208e0340188
duo le Sun <1091282902@qq.com>
2025-06-18 21:16:12 +0800

    fix

--- Banked/anchor/aimax/tests/aimax.ts
+++ Banked/anchor/aimax/tests/aimax.ts
@@ -10,9 +10,12 @@ describe("All Tests", () => {
   const senderProgram = anchor.workspace.AimaxSender as Program<AimaxSender>;
   const receiverProgram = anchor.workspace.AimaxReceiver as Program<AimaxReceiver>;
 
+
+  
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await senderProgram.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
 });
+
