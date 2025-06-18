63b72078bda37a0d5b06d4efa28aa7adf8b08d12
duo le Sun <1091282902@qq.com>
2025-06-18 09:25:26 +0800

    fix

--- Banked/anchor/aimax/tests/aimax.ts
+++ Banked/anchor/aimax/tests/aimax.ts
@@ -1,16 +1,18 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Aimax } from "../target/types/aimax";
+import { AimaxSender } from "../target/types/aimax_sender";
+import { AimaxReceiver } from "../target/types/aimax_receiver";
 
-describe("aimax", () => {
+describe("All Tests", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.aimax as Program<Aimax>;
+  const senderProgram = anchor.workspace.AimaxSender as Program<AimaxSender>;
+  const receiverProgram = anchor.workspace.AimaxReceiver as Program<AimaxReceiver>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await senderProgram.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
 });
